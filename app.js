f7b7d4691f7b9047aff4d86e1f098258f291fb3f
Marwa Isa Jaafar Ahmed <Marwa.Mohamed@HHG.com>
2025-02-08 01:24:12 +0300

    DONEE

--- app.js
+++ app.js
@@ -151,6 +151,38 @@ console.log('Exercise 2 correct result: ', [
   { first: 'Hanna', last: 'Hammarström' },
 ]);
 
+/*
+Exercise 3: Array.prototype.sort()
+
+Sort the inventors by birth date in ascending order (from those born furthest in
+the past to those born most recently).
+*/
+
+let sortedByBirthYear = [];
+
+// Complete the exercise in the space below:
+sortedByBirthYear = inventors.sort((a,b)=>{
+return a.year - b.year;
+
+})
+// Check your work:
+console.log('Exercise 3 my result: ', sortedByBirthYear);
+console.log('Exercise 3 correct result: ', [
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+  { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 },
+  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+  { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+]);
+
+
 /* 
 Exercise 4: Array.prototype.find()
 
@@ -259,10 +291,12 @@ old or older.
 */
 
 let isAdultPresent = null;
+let currentDate = new Date();
+let currentYear = currentDate. getFullYear();
 
 // Complete the exercise in the space below:
 isAdultPresent = devs.some((element)=>{
-  return (2024 - element.year)>=18;
+  return (currentYear - element.year) > 18
 
 })
 // Check your work:
@@ -287,7 +321,7 @@ let isEveryone19OrOlder = null;
 
 // Complete the exercise in the space below:
 isEveryone19OrOlder = devs.every((element)=>{
-  return (2024 - element.year)>=19
+  return (currentYear - element.year)>=19
 })
 // Check your work:
 console.log('Exercise 7 my result: ', isEveryone19OrOlder);
